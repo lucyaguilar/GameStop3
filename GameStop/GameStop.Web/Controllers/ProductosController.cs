02392ba5359fa0e5472d67e51327e6875fd6b3bf
lucyaguilar <lucywaldina12@gmail.com>
2022-06-14 16:43:12 -0600

    Agregamos una lista de productos

---
+++ GameStop/GameStop.Web/Controllers/ProductosController.cs
@@ -0,0 +1,34 @@
+﻿using GameStop.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GameStop.Web.Controllers
+{
+    public class ProductosController : Controller
+    {
+        // GET: Productos
+        public ActionResult Index()
+        {
+            var producto1 = new ProductoModel();
+            producto1.Id = 1;
+            producto1.Descripcion = "minecraft";
+
+            var producto2 = new ProductoModel();
+            producto2.Id = 2;
+            producto2.Descripcion = "spider man";
+
+            var producto3 = new ProductoModel();
+            producto3.Id = 3;
+            producto3.Descripcion = "Dragon ball z tenkashi";
+
+            var listadeProductos = new List<ProductoModel>();
+            listadeProductos.Add(producto1);
+            listadeProductos.Add(producto2);
+            listadeProductos.Add(producto3);
+            return View(listadeProductos);
+        }
+    }
+}
