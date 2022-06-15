0a737b82fd8b1860edde96e015d2386b47154c07
lucyaguilar <lucywaldina12@gmail.com>
2022-06-14 18:51:31 -0600

    Agregando arquitectura basica

--- GameStop/GameStop.Web/Controllers/ProductosController.cs
+++ GameStop/GameStop.Web/Controllers/ProductosController.cs
@@ -1,4 +1,4 @@
-﻿using GameStop.Web.Models;
+﻿using GameStop.BL;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,22 +12,8 @@ namespace GameStop.Web.Controllers
         // GET: Productos
         public ActionResult Index()
         {
-            var producto1 = new ProductoModel();
-            producto1.Id = 1;
-            producto1.Descripcion = "minecraft";
-
-            var producto2 = new ProductoModel();
-            producto2.Id = 2;
-            producto2.Descripcion = "spider man";
-
-            var producto3 = new ProductoModel();
-            producto3.Id = 3;
-            producto3.Descripcion = "Dragon ball z tenkashi";
-
-            var listadeProductos = new List<ProductoModel>();
-            listadeProductos.Add(producto1);
-            listadeProductos.Add(producto2);
-            listadeProductos.Add(producto3);
+            var productosBL = new ProductosBL();
+            var listadeProductos = productosBL.ObtenerProductos();
             return View(listadeProductos);
         }
     }
---
+++ GameStop/GameStop.WebAdmi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GameStop.WebAdmi.MvcApplication" Language="C#" %>
